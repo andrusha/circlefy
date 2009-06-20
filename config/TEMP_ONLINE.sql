2b721dfcebd9f061139df0fe2b656ae1e9980fb8
taso <taso@5cc1cc4a-9599-4f1d-a638-0094efb19ed0>
2009-06-20 21:37:06 +0000

    Added TEMP_ONNLINE table create statement

---
+++ config/TEMP_ONLINE.sql
@@ -0,0 +1,9 @@
+Create Table: CREATE TABLE `TEMP_ONLINE` (
+  `toid` int(5) NOT NULL auto_increment,
+  `uid` int(5) NOT NULL,
+  `cid` varchar(255) NOT NULL,
+  `timeout` tinyint(4) NOT NULL default '0',
+  PRIMARY KEY  (`toid`),
+  UNIQUE KEY `uid` (`uid`),
+  UNIQUE KEY `cid` (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1
