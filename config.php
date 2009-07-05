81e0ed008d2c701a6e757fc6d594bc31984ee512
taso <taso@5cc1cc4a-9599-4f1d-a638-0094efb19ed0>
2009-07-05 01:07:48 +0000

    Temp Domain Fix

--- config.php
+++ config.php
@@ -11,6 +11,8 @@ define("SIGNUP","parts/sign_up/sign_up.phtml");
 define("CATEGORY","parts/lists/category.phtml");
 define("TITLE","tap");
 
+define("DOMAIN","tap.info");
+
 define("PROFILE_PIC_REL", "/user_pics/");
 define("D_GROUP_PIC_REL", "/group_pics/");
 define("PROFILE_PIC_PATH", "/var/data/user_pics");
