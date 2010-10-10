b4218f973b8baacd78db3513c59bb154148cd96c
andrew <andrew@5cc1cc4a-9599-4f1d-a638-0094efb19ed0>
2010-10-10 15:53:53 +0000

    User page

---
+++ pages/user.php
@@ -0,0 +1,34 @@
+<?php
+
+class page_user extends Base {
+    protected $need_login = true;
+
+	function __invoke() {
+        $uname = $_GET['uname'];
+
+        $user = User::init($uname);
+        $user->stats = $user->getStats();
+
+        $this->set($user->asArray(), 'user');
+
+        $this->set(
+            GroupsList::search('byUser', array('uid' => $user->id, 'limit' => 10), G_LIMIT)
+                      ->asArrayAll(),
+            'circles');
+        
+        $this->set(
+            TapsList::search('friend', array('uid' => $user->id), T_GROUP_INFO)
+                    ->asArrayAll(),
+            'feed');
+
+        $this->set(
+            UsersList::search('followers', array('id' => $user->id, 'limit' => 14), U_LIMIT)
+                     ->asArrayAll(),
+            'followers');
+
+        $this->set(
+            UsersList::search('following', array('id' => $user->id, 'limit' => 14), U_LIMIT)
+                     ->asArrayAll(),
+            'following');
+	}
+};
